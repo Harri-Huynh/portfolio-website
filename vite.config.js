91c898de3d93c98dd301c23017e01f35b43785cd
Harri-Huynh <harrisonhuynh@outlook.com>
2025-11-26 15:08:46 -0800

    changed env.local name to repo name

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     // base: `/${env.VITE_REPO_NAME}/`,
-    base: "./",
+    base: "/portfolio-website",
     plugins: [react(), tailwindcss()],
     // server: {
     //   open: true,
