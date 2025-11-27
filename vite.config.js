08bace8badf63bf82926749dc806434a2374c29e
Harri-Huynh <harrisonhuynh@outlook.com>
2025-11-27 15:53:31 -0800

    changed base to / for custoom url

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     // base: `/${env.VITE_REPO_NAME}/`,
-    base: '/portfolio-website/',
+    // base: '/portfolio-website/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     // server: {
     //   open: true,
